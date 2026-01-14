8225f7d051eb07fa6c0eb7ed87adabdf05869a2e
forever <user@forever>
2026-01-14 20:29:52 +0300

    init commit

---
+++ src/main/java/ru/ugaforever/WebConfiguration.java
@@ -0,0 +1,10 @@
+package ru.ugaforever;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ru.ugaforever.controller")
+public class WebConfiguration {}
